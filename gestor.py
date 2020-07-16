347e1312215b84202f0487a5039991dc40a519eb
Vhartz <vhartzamorimg2@gmail.com>
2020-07-16 11:54:42 -0300

    at11:54:21

--- gestor.py
+++ gestor.py
@@ -6,25 +6,28 @@ from tkinter import messagebox
 from random import choice
 from time import strftime as time
 import os
-import shutil
-import string
+#import shutil
+import string # para selecionar os caracteres da senha
 import pyperclip as pc #https://pypi.org/project/pyperclip/
+
+__title__ = 'gestor-linux'
+__version__ = '0.1'
+__author__ = 'Vitor Amorim'
+__license__ = 'MIT 2020'
+__copyright__ = 'Copyright 2020 Vitor Amorim'
 
 root = Tk()
 root.title(" Gestor Pessoal de Configurações ")
 root.geometry("500x500")
 root.configure(bg='#3803FA')
 root.resizable(False, False)
-#root.iconbitmap('r : /home/Documentos/gestor-linux/icon.ico')
+#root.iconbitmap(r': icon.ico')
 
-#variaveis para uso interno
+''' FUNÇÔES e VARIAVEIS para uso interno '''
+
 sim = IntVar()
 nao = IntVar()
 escala = IntVar()
-#resluc = IntVar()
-#alises = IntVar()
-
-''' FUNÇÔES '''
 
 def gerado_senha(): #gerador de senha
     escala_copia = (escala).get()
@@ -36,6 +39,7 @@ def gerado_senha(): #gerador de senha
       senha += choice(letras + numeros)
     messagebox.showinfo("Senha Gerada", senha)
     pc.copy(senha)
+    print('senha criada\t' + senha)
     messagebox.showinfo("Senha Gerada", 'senha copiada!!')
 
 def atualizar_repos(): #Atualizar Repositorios mais velozes
@@ -44,8 +48,8 @@ def atualizar_repos(): #Atualizar Repositorios mais velozes
 
     if sim_s == True and nao_n == False:
         messagebox.showinfo("!", 'Aguarde alguns segundos ..')
-        os.mkdir('/log')
-        os.system('pacman-mirrors --country Brazil > /log/log_repos.txt')
+        os.mkdir('log')
+        os.system('pacman-mirrors --country Brazil > log/log_repos.txt')
         messagebox.showinfo("!", 'Repositorios Atualizados, log salvo')
     elif nao_n == True and sim_s == False:
         messagebox.showinfo("!", 'Aguarde alguns segundos ..')
@@ -61,38 +65,62 @@ def setar_hora_manjaro():
     except Exception:
         os.system('pacman -S ntp')
         return setar_hora_manjaro()
+    finally:
+        print('Data e Hora Configurados com sucesso!')
 
-def rmv_blk_arch(): #remover block do pcman no manjaro
+def rmv_blk_arch(): #remover block do pacman
     os.system('rm -rf /var/lib/pacman/db.lck')
+    print('db.lck removido!')
     messagebox.showwarning("warning","Trava removida! ")
 
-def rmv_blk_debian():
+def rmv_blk_debian(): #remover block do apt-get
     os.system('rm -rf /var/lib/apt/lists/lock')
+    print('lock removido!')
     messagebox.showwarning("warning","Trava removida! ")
 
 def sair(): # fechar janela
     root.destroy()
 
-def thunar(): # mostrar diretorio dos Scripts
+def thunar(): # abrir diretorio dos Scripts
     os.system('thunar /home/$USER')
 
-
-def sobre():
+def sobre(): # botao sobre do menu
     #messagebox para botoes instantaneos https://www.javatpoint.com/python-tkinter-messagebox
     messagebox.showinfo("Sobre","vhartzamorimg2@gmail.com")
 
-'''
-def copiar_scripts():
-    resolução_script = (resluc).get()
-    aliases_bash = (alises).get()
-
-    if resolução_script == True:
-        os.system('cp /Scripts/resolução.sh /home/$USER')
-    elif aliases_bash == True:
+def configurar_zsh(): # instalar e configurar o oh-my-zsh https://github.com/ohmyzsh/ohmyzsh
+    try:
+        os.system('pacman -S zsh zsh-autosuggestion')
+        os.system('git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting')
+        os.system('git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions')
+        os.system('sudo sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"')
         os.system('')
+        os.system('cp /Scripts/.zshrc ~/')
+        os.system('source ~/.zshrc')
+    except Exception:
+        os.system('pacman -S wget git')
+        return configurar_zsh()
+    finally:
+        print('Instalado e Configurado com Sucesso')
+
+def pacotes_basicos():
+    os.system('pacman-mirrors --country Brazil')
+    os.system('pacman -Sy && pacman -S htop speedtest-cli cmatrix git wget curl trizen vim ranger')
+    print('pacotes basicos instalados!')
+
+'''
+def pacotes_e_config_i3wm():
+    os.system('pacman -S rofi nitrogen compton ranger pcmanfm')
+    #os.system('cp /Scripts/i3.confjdhask ~/.config/i3/ .....')        
 '''
 
-# menu itens do menu
+def setar_resolução():
+    os.system('./Scripts/resolução.sh')
+    os.system('cp Scripts/resolução.sh ~/')
+    os.system('echo "exec ~/resolução.sh" >> ~/.xinitrc')
+    print('resolução configurada com sucesso')
+
+# menu e itens do menu
 menu_bar = Menu(root, font='bold')
 menu_principal = Menu(menu_bar, tearoff=0, font='bold')
 menu_principal.add_command(label='Backup')
@@ -111,7 +139,10 @@ opçao_gerador = Scale(
                      ).place(x=350, y=15)
 
 #geração de log na atualizar_repos
-boao_atualizar_repos = Button(root, text='Atualizar Repositorios', command=atualizar_repos).place(x=10,y=60)
+boao_atualizar_repos = Button(
+                             root, text='Atualizar Repositorios', command=atualizar_repos
+                             ).place(x=10,y=60)
+
 gerar_log = Label(root, text='Gerar log?', font='bold', bg='#3803FA').place(x=180, y=64)
 
 sim_log = Checkbutton(
@@ -124,8 +155,8 @@ nao_log = Checkbutton(
                      onvalue = 1, offvalue = 0, activebackground='#3803FA'
                      ).place(x=380, y=64)
 
-rmv_arch_btn = Button(root, text='Remover block arch e manjaro', command=rmv_blk_arch).place(x=10, y=100)
-rmv_deb_btn = Button(root, text='Remover block debian e ubuntu', command=rmv_blk_debian).place(x=250, y=100)
+rmv_arch_btn = Button(root, text='Remover block Pacman', command=rmv_blk_arch).place(x=10, y=100)
+rmv_deb_btn = Button(root, text='Remover block APT-GET', command=rmv_blk_debian).place(x=250, y=100)
 setar_hora = Button(root, text='Acertar Hora no Manjaro', command=setar_hora_manjaro).place(x=10, y=150)
 
 root.mainloop()
